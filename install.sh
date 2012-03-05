5491c387c6517dba042a7e81ce8b30d4159679d9
Juan Pinilla <dark@shamain.com.ar>
2012-03-04 22:42:42 -0500

    ...

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd ~ 
+git clone https://github.com/nodejitsu/haibu.git
+cd haibu/
+npm install optimist
+npm install flatiron
+npm install semver
+npm install pkginfo
+npm install npm
+npm install forever
+npm install request
+npm install cloudfiles
+npm install union
+npm install eyes
+npm install haibu-carapace
