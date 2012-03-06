5aa68a7db76590cf90d50f2011a42447ee01ec40
Bryan Garzón Bahamón <darks@dksource.com.ar>
2012-03-05 19:06:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,14 @@ su - clap
 cd ~ 
 git clone https://github.com/nodejitsu/haibu.git
 cd haibu/
-npm install optimist flatiron semver pkginfo npm forever request cloudfiles union eyes haibu-carapace
+npm install optimist
+npm install flatiron
+npm install semver 
+npm install pkginfo
+npm install npm
+npm install forever
+npm install request
+npm install cloudfiles
+npm install union
+npm install eyes
+npm install haibu-carapace
