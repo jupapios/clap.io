ce8cc2f210170dfbab7a67cb6647cc5ab7dc118f
Juan Pinilla <juan@kamikazepanda.com>
2012-04-20 13:27:32 -0500

    if you're logged dont go to register or coupon

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 require(['util/Parser'], function (Parser) {
 	new Parser().parse()
 	document.body.removeChild(document.getElementById('loader'))
+	if ($$('a').hasClass('disabled')) return
 })
--- routes/home.coffee
+++ routes/home.coffee
@@ -1,3 +1,7 @@
 exports.index = (req, res) ->
+  logged = false
+  if req.session.user
+    logged = true
   res.render "index",
     title: "clap-io - Home"
+    logged: logged
